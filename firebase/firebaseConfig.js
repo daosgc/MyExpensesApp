b590b10892f21d261f1e0cf550e87987fb135de6
Darwin Gualotuña <daosgc@gmail.com>
2020-05-19 22:08:20 -0500

    Add react-native dotenv to firestore

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,14 +1,23 @@
 import * as firebase from 'firebase';
-import firestore from 'firebase/firestore'
+
+import {
+    API_KEY,
+    AUTH_DOMAIN,
+    DATABASE_URL,
+    PROJECT_ID,
+    STORAGE_BUCKET,
+    MESSAGIN_SENDER_ID,
+    APP_ID
+} from 'react-native-dotenv'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB6fARkq_xoPnOLh4WUwEwPS5FF9lWfcCo",
-    authDomain: "training-13331.firebaseapp.com",
-    databaseURL: "https://training-13331.firebaseio.com",
-    projectId: "training-13331",
-    storageBucket: "training-13331.appspot.com",
-    messagingSenderId: "68279360369",
-    appId: "1:68279360369:web:63dfa31acceab7571bb4d0"
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    databaseURL: DATABASE_URL,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGIN_SENDER_ID,
+    appId: APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
