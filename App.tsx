ec908f02fef6356f64e2b87d00662a724c591e89
Darwin Gualotuña <daosgc@gmail.com>
2020-05-21 07:59:11 -0500

    Move navigation to AppNavigator component

--- App.tsx
+++ App.tsx
@@ -1,47 +1,8 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import { AddExpenseScreen, ExpenseScreen, ExpenseDetailScreen} from './src/screens';
-
-const Stack = createStackNavigator();
-
-function MyStack() {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-          headerStyle: {
-            backgroundColor: '#621FF7',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}
-      >
-      <Stack.Screen
-        name="AddExpenseScreen"
-        component={AddExpenseScreen}
-        options={{ title: 'Add Expense' }}
-      />
-      <Stack.Screen
-        name="ExpenseScreen"
-        component={ExpenseScreen}
-        options={{ title: 'Expenses List' }}
-      />
-      <Stack.Screen
-       name="ExpenseDetailScreen"
-       component={ExpenseDetailScreen}
-       options={{ title: 'Expense Detail' }}
-      />
-    </Stack.Navigator>
-  );
-}
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <MyStack />
-    </NavigationContainer>
+    <AppNavigator />
   );
 }
---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { AddExpenseScreen, ExpenseScreen, ExpenseDetailScreen } from '../screens';
+
+const AppNavigator = () => {
+  const Stack = createStackNavigator();
+
+  MyStack = () => {
+    return (
+      <Stack.Navigator
+        screenOptions={{
+            headerStyle: {
+              backgroundColor: '#621FF7',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        >
+        <Stack.Screen
+          name="AddExpenseScreen"
+          component={AddExpenseScreen}
+          options={{ title: 'Add Expense' }}
+        />
+        <Stack.Screen
+          name="ExpenseScreen"
+          component={ExpenseScreen}
+          options={{ title: 'Expenses List' }}
+        />
+        <Stack.Screen
+         name="ExpenseDetailScreen"
+         component={ExpenseDetailScreen}
+         options={{ title: 'Expense Detail' }}
+        />
+      </Stack.Navigator>
+    );
+  }
+
+  return(
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  )
+};
+
+export default AppNavigator;
